8a65faee16178772b1d4c752c3a3c9793f13d287
Olufemi Afolabi <olu@Olufemis-MacBook-Pro.local>
2018-10-21 13:47:41 -0500

    portfolio rebranding

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/////// TOGGLE FUNCTION /////////
+/////// TOGGLE FUNCTION /////////
+$(document).ready(function() {
+  $("#mobile_nav").click(function() {
+    if ($("#primary_nav").css("left") < "0px") {
+      $("#primary_nav").animate({ left: "0px" }, 300);
+      $("#main-display").animate({ left: "170px" }, 300);
+      $("body").css("overflow-x", "hidden");
+      $("#primary_nav").css("overflow-y", "hidden");
+    } else {
+      $("#primary_nav").animate({ left: "-170px" }, 300);
+      $("#main-display").animate({ left: "0px" }, 300);
+      $("body").css("overflow-x", "hidden");
+    }
+  });
+}); //end
+
+//Get the current year for the copyright
+$("#year").text(new Date().getFullYear());
+
+// SMOOTH SCROLL
+//init Scroll spy
+$(".body").scrollspy({ target: "#scroll-top" });
+$("#scroll-top a").on("click", function(e) {
+  if (this.hash !== "") {
+    //prevent default
+    e.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      850,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
