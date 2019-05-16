8c95b4948f8e7cfb71cf479565a719056d99d837
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-05-15 23:26:32 -0500

    navlink properties

---
+++ build/precache-manifest.024de23c87e2c825fb6cabafcf09dc16.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d44aff3af7aab4ce4495c3de9c2e64",
+    "url": "/static/media/react.84d44aff.png"
+  },
+  {
+    "revision": "e79283538683f55761cf",
+    "url": "/static/css/main.f70531e1.chunk.css"
+  },
+  {
+    "revision": "dcc0d89c242ec9410ae2e57f0e1057f2",
+    "url": "/static/media/twitterLike.dcc0d89c.png"
+  },
+  {
+    "revision": "eebafbbc1c1ea823e524",
+    "url": "/static/js/1.eebafbbc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fe7ca605f51753ab2b583720c6b3c57",
+    "url": "/static/media/github.4fe7ca60.svg"
+  },
+  {
+    "revision": "c7ae3890df97e84e4e06393088bc0a4b",
+    "url": "/static/media/eye.c7ae3890.svg"
+  },
+  {
+    "revision": "1d6d225444ae69e307dffadaf6705446",
+    "url": "/static/media/home.1d6d2254.svg"
+  },
+  {
+    "revision": "56e8af93ccdac569e66264543e191ca6",
+    "url": "/static/media/olufemiafolabi.56e8af93.docx"
+  },
+  {
+    "revision": "19e4905c1951bf9400bf7d755622a833",
+    "url": "/static/media/codepen.19e4905c.svg"
+  },
+  {
+    "revision": "1e0f24e9ad002a2c8427c76325431874",
+    "url": "/static/media/linkedin.1e0f24e9.svg"
+  },
+  {
+    "revision": "e8e4b28c8580379720978ede0659fcc5",
+    "url": "/static/media/instagram.e8e4b28c.svg"
+  },
+  {
+    "revision": "d8afcf9a4435dc2d5f108fd593787ed9",
+    "url": "/static/media/profileImage.d8afcf9a.jpg"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/JavaScript-logo.0cded3a3.png"
+  },
+  {
+    "revision": "a9d48feb8723804c550cc5f0c2ed139b",
+    "url": "/static/media/git.a9d48feb.png"
+  },
+  {
+    "revision": "cf7c31e7381f89f6b7166c55b986c161",
+    "url": "/static/media/java.cf7c31e7.jpg"
+  },
+  {
+    "revision": "bd7ab5748a52ebb286ccd891d0054782",
+    "url": "/static/media/php.bd7ab574.png"
+  },
+  {
+    "revision": "08ebf3d0bdc551aa5861bb101c767dd2",
+    "url": "/static/media/node.08ebf3d0.png"
+  },
+  {
+    "revision": "e79283538683f55761cf",
+    "url": "/static/js/main.e7928353.chunk.js"
+  },
+  {
+    "revision": "d397d85b748832c777d640e6ac0c5ae4",
+    "url": "/static/media/vscode.d397d85b.png"
+  },
+  {
+    "revision": "73e24896289a49f3365faf66faec2f65",
+    "url": "/static/media/sass.73e24896.png"
+  },
+  {
+    "revision": "88061c06fa552fb6b26abb733f6a34cd",
+    "url": "/static/media/sketch.88061c06.png"
+  },
+  {
+    "revision": "3a95ed4894f7e20a138d812e940adbbc",
+    "url": "/static/media/photoshop.3a95ed48.png"
+  },
+  {
+    "revision": "7a3de0f8759874ba5ade769944fb0102",
+    "url": "/static/media/googleanalytics.7a3de0f8.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/static/media/mongodb.e5eee315.png"
+  },
+  {
+    "revision": "653f9458ee06f3c078685bad85835786",
+    "url": "/static/media/firebase.653f9458.png"
+  },
+  {
+    "revision": "9a2092fbefbb2477a001411bd8e67c69",
+    "url": "/static/media/python.9a2092fb.jpg"
+  },
+  {
+    "revision": "0fca7fd919957aae013fdb9845f83353",
+    "url": "/static/media/css.0fca7fd9.png"
+  },
+  {
+    "revision": "f761e2baddd717c409e3fd5a98524a03",
+    "url": "/static/media/friendconnet.f761e2ba.png"
+  },
+  {
+    "revision": "e3e2b5e37feb7d51b94cbad0a2fe3c53",
+    "url": "/static/media/lyrica.e3e2b5e3.png"
+  },
+  {
+    "revision": "12db982f22bc43d678a14bea829143c1",
+    "url": "/static/media/moviedb.12db982f.png"
+  },
+  {
+    "revision": "d91278b7b515efb325bac420c25f8192",
+    "url": "/static/media/shopwiseinc.d91278b7.png"
+  },
+  {
+    "revision": "abc07aa7d63143e2035d24261e9e1bc1",
+    "url": "/static/media/pig-game.abc07aa7.png"
+  },
+  {
+    "revision": "eebafbbc1c1ea823e524",
+    "url": "/static/css/1.8307199d.chunk.css"
+  },
+  {
+    "revision": "57c0cc41461aeb14e9c04284ac0e037c",
+    "url": "/index.html"
+  }
+];
