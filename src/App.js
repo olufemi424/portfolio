eeda4f8b38ac06046cea5d3e3d60545ddb60b40c
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-02-06 20:22:56 -0600

    add gfx page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
 } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import Home from "./pages/Home";
+import Graphics from "./pages/Graphics";
 
 //page not found
 import PageNotFound from "./layouts/PageNotFound";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/404" component={PageNotFound} />
-            <Redirect from="*" to="/404" />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/graphics" component={Graphics} />
+						<Route path="/404" component={PageNotFound} />
+						<Redirect from="*" to="/404" />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/pages/Graphics.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class Graphics extends Component {
+	state = {
+		data: {},
+		loaded: false
+	};
+
+	componentDidMount() {
+		this.getData();
+	}
+
+	getData = async () => {
+		await axios
+			.get(`https://www.instagram.com/olugfx/?__a=1`)
+			.then(({ status, data }) => {
+				if (status === 200) {
+					this.setState({ data: data, loaded: true });
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	render() {
+		const { loaded } = this.state;
+		let userData;
+		if (loaded) {
+			userData = this.state.data.graphql.user;
+		}
+
+		console.log(userData);
+		return (
+			<div style={{ width: "80%", margin: "80px auto" }}>
+				{loaded ? (
+					<div>
+						<div style={{ display: "flex", alignContent: "center" }}>
+							<img
+								src={userData.profile_pic_url_hd}
+								alt={userData.full_name}
+								width="100"
+								height="100"
+							/>
+							<ul style={{ color: "#000" }}>
+								<li>Description: {userData.biography}</li>
+								<li>Full Name: {userData.full_name}</li>
+								<li>followers: {userData.edge_followed_by.count}</li>
+								<li>{userData.edge_owner_to_timeline_media.count}</li>
+							</ul>
+						</div>
+
+						<div
+							style={{
+								display: "flex",
+								flexWrap: "wrap"
+							}}
+						>
+							{userData.edge_owner_to_timeline_media.edges.map(media => (
+								<div
+									key={media.node.id}
+									style={{
+										width: "300px",
+										marginRight: "30px",
+										marginBottom: "30px",
+										overflow: "hidden",
+										backgroundColor: "#f8f8f8",
+										boxShadow: "0 0 2px 1px rgba(0,0,0,.2)"
+									}}
+								>
+									<img
+										src={media.node.display_url}
+										alt={media.node.full_name}
+										width="300"
+										height="300"
+										style={{
+											marginBottom: "20px"
+										}}
+									/>
+									<p style={{ color: "#000", width: "100%", padding: "10px" }}>
+										{media.node.edge_media_to_caption.edges[0].node.text}
+									</p>
+								</div>
+							))}
+						</div>
+					</div>
+				) : (
+					<h2 style={{ color: "#000" }}>Loading...</h2>
+				)}
+			</div>
+		);
+	}
+}
+
+export default Graphics;
