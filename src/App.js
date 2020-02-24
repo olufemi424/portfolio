318ba97342f99bb8973ffd18e280f6f76b75abca
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-02-23 21:32:55 -0600

    update about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
 } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
@@ -13,21 +13,21 @@ import Home from "./pages/Home";
 import PageNotFound from "./layouts/PageNotFound";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/404" component={PageNotFound} />
-            <Redirect from="*" to="/404" />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/404" component={PageNotFound} />
+						<Redirect from="*" to="/404" />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
